df91943126fc3302b3100d686fae3d0e067f6c2f
Takuto Yanagida <yanagida@stxst.com>
2020-06-04 10:54:28 +0900

    Fix a bug
    (cherry picked from commit 4517c09e212e9436ea7f51e3f6a0f7fe59c308d6)

--- src/bimeson-list/bm-list.php
+++ src/bimeson-list/bm-list.php
@@ -102,7 +102,7 @@ class Bimeson_List {
 	public function _cb_save_post( $post_id ) {
 		if ( ! isset( $_POST['bimeson_list_nonce'] ) || ! wp_verify_nonce( $_POST['bimeson_list_nonce'], 'bimeson_list' ) ) return;
 		if ( ! current_user_can( 'edit_post', $post_id ) ) return;
-		\st\media_picker\save_post( $post_id, self::FLD_MEDIA );
+		\st\media_picker\save_items( $post_id, self::FLD_MEDIA );
 
 		$json_items = $_POST[ self::FLD_ITEMS ];
 		if ( $json_items !== self::NOT_MODIFIED ) {
